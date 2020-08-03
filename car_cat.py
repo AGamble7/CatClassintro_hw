8b4280d4fb1c3270beefcf98f75cedbfc3258883
Aliyah Gamble <shardaegamble@gmail.com>
2020-08-03 16:03:37 -0400

    created a cat class

---
+++ car_cat.py
@@ -0,0 +1,15 @@
+class car:
+    make = Honda 
+    model = Civic 
+    color = white 
+
+def __init__(self, make, model, color):
+    self.make = "Honda"
+    self.model = "Civic"
+    self.color = "white"
+
+print(" My car is a %s %d and it is %d so I call it snowflake")
+
+
+
+
